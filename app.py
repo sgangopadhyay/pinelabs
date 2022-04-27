b50cf2e1e74edb5dec2b96453e3f80099e11eb04
Suman Gangopadhyay <linuxgurusuman@gmail.com>
2022-04-27 09:35:22 +0530

    v2

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ print("Suman Gangopadhyay from Master Branch")
 print("A new line is being added for the Development Branch")
 
 # The code can be published in the MAIN Production environement 
+
+# Added the Suman() function 
+def Suman():
+  pass
